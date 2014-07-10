743c378f2da896b28863850b23dfdbcad6c7d274
andywww <andrew_wildridge@hotmail.com>
2014-07-10 15:40:10 +0100

    update

--- web.js
+++ web.js
@@ -17,10 +17,10 @@ http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
 
 	if (path == "/") {
-		res.end("Hello World. You are requestor edit # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
+		res.end("Hello Cruel World. I am the requestor edit # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
 
 	} else if (path == "/page2") {
-		res.end("This is page 2. <a href='/'>Home.</a>\n"); // send response and close connection	
+		res.end("This is page 2. <a href='/'>Here.</a>\n"); // send response and close connection	
 	}
 
 }).listen(port);
