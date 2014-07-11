aeb7a76cf2a2c0e3128a7fd9ea2762c523f06b19
andywww <andrew_wildridge@hotmail.com>
2014-07-11 11:45:41 +0100

    app2

--- app2.js
+++ app2.js
@@ -1,21 +1,22 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , url = require('url')
+var app = require('http').createServer(handler);
+//  , io = require('socket.io').listen(app)
+//  , fs = require('fs')
+//  , url = require('url')
 
-app.listen(3000);
+app.listen(process.env.PORT || 8000);
 
 
 function handler (req, res) {
 
-        fs.readFile(__dirname + req.url,
-        function (err, data) {
-          if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-          }
+        var path = req.url;
+  console.log("requested=" + path + " counter=" + counter);
 
-          res.writeHead(200);
-          res.end(data);
-        });
+  res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
+
+  if (path == "/") {
+    res.end("Hello World. I am the requestor edit # " + counter + ".<br><a href='/page2'>Page 2</a>\n");
+
+  } else if (path == "/page2") {
+    res.end("This is page 2. <a href='/'>Here.</a>\n"); // send response and close connection 
+  }
 }
